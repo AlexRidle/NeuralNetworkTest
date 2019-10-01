20b3d661ad729c7d339adec723d983bc0bc72694
Alexander <AlexanderBely98@gmail.com>
2019-10-01 12:14:03 +0300

    Copypasted 4nd lesson with awful code

--- src/main/java/com/example/Driver.java
+++ src/main/java/com/example/Driver.java
@@ -11,11 +11,9 @@ import javafx.stage.Stage;
 
 import java.util.stream.IntStream;
 
-public class Driver extends Application {
+public class Driver {
 
-    static double m = 0;
-    static double b = 0;
-    static int[][][] data = Perceptron.andData;
+    static int[][][] data = Perceptron.TRAINING_DATA;
 
     public static void main(String[] args) {
         double[] weights = Perceptron.INITIAL_WEIGHTS;
@@ -28,7 +26,7 @@ public class Driver extends Application {
         double[] adjustedWeights = null;
 
         while (errorFlag) {
-            driver.printHeading(epochNumber++);
+            driver.printHeading(epochNumber++, weights.length);
             errorFlag = false;
             error = 0;
             for (final int[][] datum : data) {
@@ -44,87 +42,51 @@ public class Driver extends Application {
             }
         }
 
-        m = -weights[2] / weights[1];
-        b = (Perceptron.THRESHOLD / weights[1]) - (weights[0]/weights[1]);
-        System.out.println("\ny =" + String.format("%.2f", m) + " x = " + String.format("%.2f", b));
-        launch(args);
+        System.out.println("\n Plane equation: " + String.format("%.2f", weights[1]) + "x + " + String.format("%.2f", weights[2]) +
+                "y + " + String.format("%.2f", weights[3]) + "z + "  + String.format("%.2f", weights[0] - Perceptron.THRESHOLD) + " = 0");
     }
 
-    public void start(final Stage primaryStage) throws Exception {
-        primaryStage.setTitle("NeuralNetworkTest");
-        final XYChart.Series<Number, Number> series1 = new XYChart.Series<Number, Number>();
-        series1.setName("zero");
-        final XYChart.Series<Number, Number> series2 = new XYChart.Series<Number, Number>();
-        series2.setName("one");
-        IntStream.range(0, Perceptron.andData.length).forEach(i -> {
-            int x = Perceptron.andData[i][0][1], y = Perceptron.andData[i][0][2];
-            if (Perceptron.andData[i][1][0] == 0) series1.getData().add(new XYChart.Data<Number, Number>(x, y));
-            else series2.getData().add(new XYChart.Data<Number, Number>(x, y));
-        });
-        double x1 = 0, y1 = b, x2 = -(b / m), y2 = 0;
-        String title = new String("y = " + String.format("%.2f", m) + "x = " + String.format("%.2f", b)
-                + " | (0, " + String.format("%.2f", b) + ") | (" + String.format("%.2f", -(b / m)) + ", 0)");
-        NumberAxis xAxis = new NumberAxis(0, 4, 1);
-        NumberAxis yAxis = new NumberAxis(0, 4, 1);
-        ScatterChart<Number, Number> scatterChart = new ScatterChart<>(xAxis, yAxis);
-        scatterChart.setTitle(title);
-        scatterChart.getData().add(series1);
-        scatterChart.getData().add(series2);
+    private void printHeading(int epochNumber, int numbOfWeights) {
+        System.out.print("\n");
+        IntStream.range(0, 64).forEach(i -> System.out.print("="));
+        System.out.print("Epoch # " + epochNumber);
+        IntStream.range(0, 64).forEach(i -> System.out.print("="));
+        System.out.print("\n");
 
-        XYChart.Series<Number, Number> series3 = new XYChart.Series<>();
-        series3.getData().add(new XYChart.Data<>(x1, y1));
-        series3.getData().add(new XYChart.Data<>(x2, y2));
-
-        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-        lineChart.getData().add(series3);
-        lineChart.setOpacity(0.1);
-
-        Pane pane = new Pane();
-        pane.getChildren().addAll(scatterChart, lineChart);
-        primaryStage.setScene(new Scene(pane, 500, 400));
-        primaryStage.show();
-    }
-
-    private void printHeading(int epochNumber) {
-        System.out.println("\n========================================== Epoch # " + epochNumber + " ===============================================");
-        System.out.println("  w0   |  w1   |  w2   |  x0  |  x1  |  x2  | Target result | Result | error | Weighted sum | adjusted w1 | adjusted w2");
-        System.out.println("----------------------------------------------------------------------------------------------------");
+        IntStream.range(0, numbOfWeights).forEach(i -> System.out.print("  w" + i++ + "  |"));
+        System.out.print(" x0 | x | y | z |Target result|Result|error|Weighted sum|");
+        IntStream.range(0, numbOfWeights-1).forEach(i -> System.out.print("adjusted w" + i++ + "  |"));
+        System.out.println("adjusted w"+ (numbOfWeights-1));
+        IntStream.range(0, 137).forEach(i -> System.out.print("-"));
+        System.out.print("\n");
     }
 
     private void printVector(int[][] data, double[] weights, int result, double error, double weightedSum, double[] adjustedWeights) {
-        String w0 = String.format("%.2f", weights[0]);
-        if(weights[0] >= 0) w0 = " " + w0;
-        String w1 = String.format("%.2f", weights[1]);
-        if(weights[1] >= 0) w1 = " " + w1;
-        String w2 = String.format("%.2f", weights[2]);
-        if(weights[2] >= 0) w2 = " " + w2;
-        String x0 = Integer.toString(data[0][0]);
-        String x1 = Integer.toString(data[0][1]);
-        String x2 = Integer.toString(data[0][2]);
+        String[] stringWeights = new String[weights.length];
+        for(int i = 0; i < weights.length; i++){
+            stringWeights[i] = String.format("%.2f", weights[i]);
+            if(weights[i] >= 0) stringWeights[i] = " " + stringWeights[i];
+        }
+        String[] stringData = new String[data[0].length];
+        for(int i = 0; i < stringData.length; i++){
+            stringData[i] = Integer.toString(data[0][i]);
+            if(data[0][i] >= 0) stringData[i] = " " + stringData[i];
+        }
         String stringError = Double.toString(error);
-        if(error >= 0) stringError = " " + stringError;
+        if(error >=0) stringError = " " + stringError;
         String stringWeightedSum = String.format("%.2f", weightedSum);
-        if(weightedSum >= 0) stringWeightedSum = " " + stringWeightedSum;
-        String adjustedw0 = String.format("%.2f", adjustedWeights[0]);
-        if(adjustedWeights[0] >= 0) adjustedw0 = " " + adjustedw0;
-        String adjustedw1 = String.format("%.2f", adjustedWeights[1]);
-        if(adjustedWeights[1] >= 0) adjustedw1 = " " + adjustedw1;
-        String adjustedw2 = String.format("%.2f", adjustedWeights[2]);
-        if(adjustedWeights[2] >= 0) adjustedw2 = " " + adjustedw2;
-        System.out.println(
-                " " + w0 +
-                        " | " + w1 +
-                        " | " + w2 +
-                        " |  " + x0 +
-                        "  |  " + x1 +
-                        "  |  " + x2 +
-                        "  |       " + data[1][0] +
-                        "       |   " + result +
-                        "    |  " + stringError +
-                        "  |     " + stringWeightedSum +
-                        "     |     " + adjustedw0 +
-                        "     |     " + adjustedw1 +
-                        "    | " + adjustedw2
-        );
+        if(weightedSum >=0) stringWeightedSum = " " + stringWeightedSum;
+        String[] stringAdjustedWeights = new String[adjustedWeights.length];
+        for(int i = 0; i < adjustedWeights.length; i++){
+            stringAdjustedWeights[i] = String.format("%.2f", adjustedWeights[i]);
+            if(adjustedWeights[i] >= 0) stringAdjustedWeights[i] = " " + stringAdjustedWeights[i];
+        }
+        IntStream.range(0, stringWeights.length).forEach(i -> System.out.print(stringWeights[i]+ " |"));
+        System.out.print(" ");
+        IntStream.range(0, stringData.length).forEach(i -> System.out.print(stringData[i]+ " |"));
+        System.out.print("      "+ data[1][0] + "      |   " + result +"  |" + stringError + " |   " + stringWeightedSum+"    |     ");
+        IntStream.range(0, stringAdjustedWeights.length-1).forEach(i -> System.out.print(stringAdjustedWeights[i] + "   |   "));
+        System.out.println(stringAdjustedWeights[stringAdjustedWeights.length-1]);
+
     }
 }
--- src/main/java/com/example/Perceptron.java
+++ src/main/java/com/example/Perceptron.java
@@ -2,14 +2,14 @@ package com.example;
 
 class Perceptron {
     public static final double THRESHOLD = 0.0;
-    static final int[][][] andData = {
-            {{1, 0, 0}, {0}},
-            {{1, 0, 1}, {0}},
-            {{1, 1, 0}, {0}},
-            {{1, 1, 1}, {1}}
+    static final int[][][] TRAINING_DATA = {
+            {{1, 1, 1, 1}, {1}},
+            {{1, 2, 2, 2}, {1}},
+            {{1, 1, -1, -1}, {0}},
+            {{1, 2, -2, -2}, {0}}
     };
     private static final double LEARNING_RATE = 0.05;
-    static final double[] INITIAL_WEIGHTS = {Math.random(), Math.random(), Math.random()};
+    static final double[] INITIAL_WEIGHTS = {Math.random(), Math.random(), Math.random(), Math.random()};
 
     double calculateWeightedSum(int[] data, double[] weights) {
         double weightedSUm = 0;
